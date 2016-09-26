0f42c1c85e9ee14aebd78cb02be9e250ad4a01f0
Izaak Khaliq <(none)>
2016-09-26 22:32:57 +0800

    Add readme

---
+++ README.md
@@ -0,0 +1,31 @@
+Programming Challenge 101
+=========================
+
+A)
+Write a program that will generate four(4) types of printable random
+objects and store them in a single file, each object will be separated by
+a ",".  These are the 4 objects: alphabetical strings, real numbers,
+integers, alphanumerics. The alphanumerics should contain a random
+number of spaces before and after it (not exceeding 10 spaces).
+The output should be 10MB in size.
+
+Sample extracted output :
+
+hisadfnnasd, 126263, assfdgsga12348fas, 13123.123,
+lizierdjfklaasf, 123192u3kjwekhf, 89181811238,122,
+nmarcysfa900jkifh  , 3.781, 2.11, ....
+
+
+B)
+Create a program that will read the generated file above and print to
+the console the object and its type. Spaces before and after the
+alphanumeric object must be stripped.
+
+Sample output :
+
+youruasdifafasd - alphabetical strings
+127371237 - integer
+asdfka12348fas - alphanumeric
+13123.123 - real numbers
+asjdfklasdjfklaasf - alphabetical strings
+123192u3kjwekhf - alphanumeric
--- writer/program.js
+++ writer/program.js
@@ -5,6 +5,19 @@ var Random = require('random-js')()
 
 var randomString = randomstring.generate()
 
+var getRemainingFilesize = function(){
+  // Return filesize in B
+  var stats = fs.statSync('./reader/example.txt')
+  return 1000001.0 - stats["size"] //uncomment for MB / 1000000.0
+}
+
+var getCurrentContents = function(){
+  fs.readFile('./output/output.txt', 'utf8', function(err, content){
+    if (err) throw console.log('Error reading current file', err)
+    return content
+  })
+}
+
 var genAlphabeticString = function(){
   return randomstring.generate({
     // to generate limited length based on filesize
@@ -21,6 +34,7 @@ var genIntegers = function(){
 }
 
 var genAlphanumeric = function(){
+  // var spaceBefore = for (i=0; i<=)
   return randomstring.generate({
     // to generate limited length based on filesize
     charset: 'alphanumeric'
@@ -32,9 +46,7 @@ var randRealNumber = genRealNumber()
 var randInteger = genIntegers()
 var randAlphanumeric = genAlphanumeric()
 
-// Get filesize in MB
-var stats = fs.statSync('./reader/example.txt')
-var fileSizeInMegaBytes = stats["size"] / 1000000.0
+var fileSizeInMegaBytes = getRemainingFilesize()
 
 fs.writeFile('./output/output.txt', fileSizeInMegaBytes, function(err, content){
   if (err) throw console.error('Error writing output', err);
